44062b2d90c12e2c2b3e1cc5d5814b7bbcff7820
Romjan Ali <romjanvr5@gmail.com>
2025-02-03 00:46:20 +0600

    Removed debug-toolbar import

--- taskify/urls.py
+++ taskify/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core.views import home, no_permission
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
