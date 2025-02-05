3e9d6577a9456b84bed2cbcd91aa92820afd8034
Romjan Ali <romjanvr5@gmail.com>
2025-02-05 12:48:07 +0600

    Added default image for profile and fixed navbar design

--- users/models.py
+++ users/models.py
@@ -3,12 +3,15 @@ from django.contrib.auth.models import AbstractUser
 from cloudinary.models import CloudinaryField
 # Create your models here.
 
+DEFAULT_PROFILE_IMAGE = "https://res.cloudinary.com/dwbggpcgf/image/upload/v1738515996/oywv0ctnqnjvbi2ypcrt.png"
+
 
 class CustomUser(AbstractUser):
     profile_image = CloudinaryField(
         "profile_image",
         blank=True,
-        null=True
+        null=True,
+        default=DEFAULT_PROFILE_IMAGE
     )
     bio = models.TextField(blank=True)
 
